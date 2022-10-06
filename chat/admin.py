c1703e68b544e3bcc8c4db9c209167d9f100dcf5
yasinkoker <yasinkoker@papilon.com.tr>
2022-10-06 20:04:29 +0300

    tables are created.

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from chat import models
+
+class ConfigUser(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'username')
+
+class ConfigWebSocket(admin.ModelAdmin):
+    list_display = ('id', 'room_name', 'user_websocket' ,'timestamp')
+
+admin.site.register(models.User, ConfigUser)
+admin.site.register(models.WebSocket, ConfigWebSocket)
