2ac13e5ca4371311a6a93e6afb1b973a5a7ee6ec
yasinkoker <yasinkoker@papilon.com.tr>
2022-10-06 15:12:22 +0300

    changes

--- chat/consumers.py
+++ chat/consumers.py
@@ -7,6 +7,7 @@ from django.shortcuts import render, redirect
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        """make first connection with the every client"""
         # extracting information from the routing.py
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
@@ -16,28 +17,23 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             self.channel_name,
         )
 
+        # accept the get request and make handshake
         await self.accept()
 
+        # send credentials to browser
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'send_credentials',
+                'type': 'send_credentials_to_browser',
                 'room_name': self.room_name,
-                'room_group_name': self.room_group_name,
-              #  'channel_name': self.channel_name,
             }
         )
 
 
-    # 'type': 'tester_message',
-    async def send_credentials(self, event):
+    async def send_credentials_to_browser(self, event):
         room_name = event['room_name']
-        room_group_name = event['room_group_name']
-       # channel_name = event['channel_name']
         await self.send(text_data=json.dumps({
             'room_name': room_name,
-            'room_group_name': room_group_name,
-           # 'channel_name': channel_name,
         }))
 
 
@@ -51,18 +47,31 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         """Handle incoming websocket data"""
 
-        """coming from the browser"""
+        # coming from the browser
         text_data_json = json.loads(text_data)
         data_location = text_data_json['data_location']
-        message = text_data_json['message']
-        user = text_data_json['user']
 
         if(data_location == 'browser'):
+            message = text_data_json['message']
+            
             await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chatroom_message',
-                'message': message,
+                'message': "hello browser",
+            }
+        )
+
+        # getting user data from mobile 
+        if(data_location == 'mobile'):
+            user = text_data_json['user']
+
+            print("user is: %s" % user)
+
+            await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'mobile_handler',
                 'user': user,
             }
         )
@@ -71,9 +80,15 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
 
     async def chatroom_message(self, event):
         message = event['message']
-        user = event['user']
 
         await self.send(text_data=json.dumps({
             'message': message,
-            'user': user,
         }))
+
+    async def mobile_handler(self, event):
+        print("inside of mobile handler")
+        user = event['user']
+
+        await self.send(text_data=json.dumps({
+            'user': user,
+        }))    
